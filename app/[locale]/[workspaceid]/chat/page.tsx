c73da6da720f98c248a764063f7bdaae75a34451
Nights <robertsantos.dom@gmail.com>
2024-10-17 03:00:15 +0800

    Refactor chat page and add chat settings component

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -9,6 +9,7 @@ import { ChatbotUIContext } from "@/context/context"
 import useHotkey from "@/lib/hooks/use-hotkey"
 import { useTheme } from "next-themes"
 import { useContext } from "react"
+import { ChatSettings } from "@/components/chat/chat-settings"
 
 export default function ChatPage() {
   useHotkey("o", () => handleNewChat())
@@ -31,6 +32,9 @@ export default function ChatPage() {
           </div>
 
           <div className="flex grow flex-col items-center justify-center" />
+          <div className="absolute right-2 top-2">
+            <ChatSettings />
+          </div>
 
           <div className="w-full min-w-[300px] items-end px-2 pb-3 pt-0 sm:w-[600px] sm:pb-8 sm:pt-5 md:w-[700px] lg:w-[700px] xl:w-[800px]">
             <ChatInput />
